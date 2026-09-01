60bda4068e585d14ea4e86c12ce833d1b0250dea
chadhall1313 <chall12@byupathway.edu>
2026-09-01 14:15:16 -0600

    fix program

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,8 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
-
         Console.WriteLine("What is your first name?");
         string first = Console.ReadLine();
         Console.WriteLine("What is your last name?");
