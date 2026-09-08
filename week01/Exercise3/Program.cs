a06aec136163a8693129e5a859671fd3fde0f4d4
chadhall1313 <chall12@byupathway.edu>
2026-09-07 19:10:48 -0600

    fixed error to give random number 1-100

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -8,7 +8,7 @@ class Program
         while (keepPlaying == "yes")
         {
             Random randomGenerator = new Random();
-            int correctNumber = randomGenerator.Next(1, 100);
+            int correctNumber = randomGenerator.Next(1, 101);
             int guess = -1;
             int guessCount = 0;
 
