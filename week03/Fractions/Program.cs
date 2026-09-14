27a6da94a80c21b16bc9ca0cf0331c624b7d388a
chadhall1313 <chall12@byupathway.edu>
2026-09-14 13:12:10 -0600

    learning activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f = new Fraction();
+        Fraction r = new Fraction(6);
+        Fraction a = new Fraction(6, 7);
+        Console.WriteLine(f.GetDecimalValue());
+        Console.WriteLine(f.GetFractionString());
+        Console.WriteLine(r.GetDecimalValue());
+        Console.WriteLine(r.GetFractionString());
+        Console.WriteLine(a.GetDecimalValue());
+
+        
     }
 }
